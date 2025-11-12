3b5926a18a4040bcf3124e0e2ae50d723f654bd7
Ben Aitcheson <aitcbj1@student.op.ac.nz>
2025-11-12 22:10:50 +1300

    Weapon sway, and boxes

--- try-this-one-ben/scripts/init_weapon.gd
+++ try-this-one-ben/scripts/init_weapon.gd
@@ -6,6 +6,8 @@ extends Node3D
 
 var weapon_instance : Node3D
 
+var mouse_movement : Vector2
+
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		load_weapon()
@@ -23,6 +25,8 @@ func _input(event) -> void:
 	if event.is_action_pressed("weapon4"):
 		WEAPON_TYPE = load("res://model/Weapon/WeaponResources/Sniper.tres")
 		load_weapon()
+	if event is InputEventMouseMotion:
+		mouse_movement = event.relative
 
 func load_weapon() -> void:
 
@@ -43,3 +47,15 @@ func load_weapon() -> void:
 	inst.rotation_degrees = WEAPON_TYPE.rotation
 
 	weapon_instance = inst
+
+func sway_weapon(delta) -> void:
+	mouse_movement = mouse_movement.clamp(WEAPON_TYPE.sway_min, WEAPON_TYPE.sway_max)
+	
+	position.x = lerp(position.x, WEAPON_TYPE.position.x - (mouse_movement.x * WEAPON_TYPE.sway_amount_position) * delta, WEAPON_TYPE.sway_speed_position)
+	position.y = lerp(position.y, WEAPON_TYPE.position.y + (mouse_movement.y * WEAPON_TYPE.sway_amount_position) * delta, WEAPON_TYPE.sway_speed_position)
+	
+	rotation_degrees.y = lerp(rotation_degrees.y, WEAPON_TYPE.rotation.y + (mouse_movement.x * WEAPON_TYPE.sway_amount_rotation) * delta, WEAPON_TYPE.sway_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, WEAPON_TYPE.rotation.x - (mouse_movement.y * WEAPON_TYPE.sway_amount_rotation) * delta, WEAPON_TYPE.sway_speed_rotation)
+
+func _physics_process(delta: float) -> void:
+	sway_weapon(delta)
