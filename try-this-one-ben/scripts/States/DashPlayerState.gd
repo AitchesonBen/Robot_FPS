08774b5cd2120093ecc02ac1bbd7c47c63d048c5
Ben Aitcheson <aitcbj1@student.op.ac.nz>
2026-01-26 00:54:31 +1300

    Fixed dashing better this time, let animations on guns cancel or not cancel with each other

--- try-this-one-ben/scripts/States/DashPlayerState.gd
+++ try-this-one-ben/scripts/States/DashPlayerState.gd
@@ -1,128 +1,52 @@
-#class_name DashPlayerState extends PlayerMovementState
-#
-#@export var DASH_SPEED := 35.0
-#@export var DASH_DECELERATION := 55.0
-#@export var DASH_DURATION := 0.15
-#
-#var dash_timer := 0.0
-#var dash_dir: Vector3
-#var dash_velocity: Vector3
-#
-#func enter(_previous_state) -> void:
-	#dash_timer = DASH_DURATION
-	#PLAYER._speed = PLAYER.SPEED_DEFAULT
-	#PLAYER.velocity.y = 0
-	#Global.has_dashed = true
-	#
-	#var input_dir := Input.get_vector(
-		#"move_left",
-		#"move_right",
-		#"move_forward",
-		#"move_backward"
-	#)
-	#
-	##dash_dir = -PLAYER.transform.basis.z
-	#if input_dir.length() >= 0.1:
-		#dash_dir = (PLAYER.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#else:
-		#dash_dir = -PLAYER.transform.basis.z
-#
-	#dash_velocity = dash_dir * DASH_SPEED
-	#PLAYER.velocity.x = dash_velocity.x
-	#PLAYER.velocity.z = dash_velocity.z
-	#
-	#PLAYER.floor_snap_length = 0.8
-#
-#func exit() -> void:
-	#pass
-#
-#func update(delta: float) -> void:
-	#dash_timer -= delta
-#
-	#dash_velocity.x = move_toward(PLAYER.velocity.x, 0, DASH_DECELERATION * delta)
-	#dash_velocity.z = move_toward(PLAYER.velocity.z, 0, DASH_DECELERATION * delta)
-	#
-	#PLAYER.velocity.x = dash_velocity.x
-	#PLAYER.velocity.z = dash_velocity.z
-#
-	#PLAYER.update_velocity()
-#
-	#if dash_timer <= 0:
-		#if PLAYER.velocity.y > 0.0:
-			#transition.emit("FallingPlayerState")
-		#else:
-			#transition.emit("IdlePlayerState")
+class_name DashPlayerState extends PlayerMovementState
 
-
-class_name DashPlayerState
-extends PlayerMovementState
-
-@export var DASH_SPEED := 35.0
+@export var DASH_SPEED := 45.0
+#Not being used
 @export var DASH_DECELERATION := 55.0
-@export var DASH_DURATION := 0.15
+@export var DASH_DURATION := 0.18
+@export var DASH_REDUCER := 0.4
 
 var dash_timer := 0.0
-var dash_direction := Vector3.ZERO
-var dash_velocity := Vector3.ZERO
+var dash_dir: Vector3
+var dash_velocity: Vector3
+#Not being used
+var dash_dec
 
 func enter(_previous_state) -> void:
 	dash_timer = DASH_DURATION
-	PLAYER._speed = PLAYER.SPEED_DEFAULT
+
+	dash_dir = get_dash_direction()
+	dash_dir = dash_dir.normalized()
+	dash_velocity = dash_dir * DASH_SPEED
+
+	PLAYER.velocity = dash_velocity
+	
 	PLAYER.floor_snap_length = 0.8
 
-	# Preserve vertical velocity if falling or jumping
-	# If you want dash to cancel Y, set PLAYER.velocity.y = 0 instead
-	PLAYER.velocity.y = 0
-	var current_y_velocity = PLAYER.velocity.y
-
-	# Get input direction
-	var input_dir := Input.get_vector(
-		"move_left",
-		"move_right",
-		"move_forward",
-        "move_backward"
-	)
-
-	# Determine dash direction
-	if input_dir.length() >= 0.1:
-		dash_direction = (PLAYER.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	else:
-		# Default forward if no input
-		dash_direction = -PLAYER.transform.basis.z
-
-	dash_velocity = dash_direction * DASH_SPEED
-	dash_velocity.y = current_y_velocity  # preserve vertical motion
-
-	# Apply initial dash
-	PLAYER.velocity = dash_velocity
-
-	# Flag that player has dashed
-	Global.has_dashed = true
-
-
 func exit() -> void:
-	# Reset anything if needed
-	pass
-
+	PLAYER.velocity *= DASH_REDUCER
 
 func update(delta: float) -> void:
 	dash_timer -= delta
 
-	# Decelerate dash velocity smoothly
-	dash_velocity.x = move_toward(dash_velocity.x, 0, DASH_DECELERATION * delta)
-	dash_velocity.z = move_toward(dash_velocity.z, 0, DASH_DECELERATION * delta)
-
-	# Apply dash velocity directly (ignore input)
 	PLAYER.velocity.x = dash_velocity.x
 	PLAYER.velocity.z = dash_velocity.z
+	PLAYER.velocity.y = 0.0
 
-	# Update vertical velocity with gravity or other vertical logic
 	PLAYER.update_velocity()
 
-	# Dash ends
 	if dash_timer <= 0:
-		# If player is falling or jumping
-		if PLAYER.velocity.y > 0.0 or !PLAYER.is_on_floor():
+		if PLAYER.velocity.y > 0.0:
 			transition.emit("FallingPlayerState")
 		else:
 			transition.emit("IdlePlayerState")
+
+func get_dash_direction() -> Vector3:
+	var input := Vector3.ZERO
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input.z = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+
+	if input.length() > 0.1:
+		return PLAYER.global_transform.basis * input
+	else:
+		return -PLAYER.global_transform.basis.z
--- try-this-one-ben/scripts/message_bus.gd
+++ try-this-one-ben/scripts/message_bus.gd
@@ -7,5 +7,15 @@ signal unlock_door(door: Array[DoorComponent])
 signal no_lock(door: Array[DoorComponent])
 signal raycastResult(enemy: Node, limb: Node)
 signal limbDamage(limb: Node, damage: int)
-signal ammo_count(name: String, ammo: int, ammoCap: int)
 signal fired
+signal ammo_count(name: String, ammo: int, ammoCap: int)
+
+var current_weapon := ""
+var ammo := 0
+var ammoCap := 0
+
+func set_ammo(name, a, c):
+	current_weapon = name
+	ammo = a
+	ammoCap = c
+	ammo_count.emit(name, a, c)
